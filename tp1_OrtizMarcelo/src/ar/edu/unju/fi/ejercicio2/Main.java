f7218cb737ce99e83ba4a721becd9321469c098f
Marce <Marce@LAPTOP-CR1JCHBA>
2024-04-13 19:01:57 -0300

    ejercicio 2

---
+++ tp1_OrtizMarcelo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio2;
+import java.lang.Math;
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "Argentina";
+		int edad = 95;
+		Double altEdificio = 45.7;
+		Double precioProducto = 17.99;
+		String numTel = "4245985";
+		Double calCoseno = Math.cos(0.5);
+		
+		System.out.println("Nombre de mi país: "+ pais);
+		System.out.println("Edad de una persona random: "+ edad);
+		System.out.println("Altura de un edifio random: "+ altEdificio);
+		System.out.println("Precio de un producto random: "+ precioProducto);
+		System.out.println("Número de teléfono random: "+ numTel);
+		System.out.println("Cálculo de coseno de 0.5: "+ calCoseno);
+		
+
+	}
+
+}
