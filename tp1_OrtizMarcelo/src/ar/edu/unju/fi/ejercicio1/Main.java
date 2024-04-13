9e168d08b7d76ec0b238f466dba0006096ff5634
Marce <Marce@LAPTOP-CR1JCHBA>
2024-04-13 18:15:05 -0300

    Se agrega paquete y clase ejercicio 1

---
+++ tp1_OrtizMarcelo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 4, n2 = 3, n3 = 6, n4 = 9, n5 = 5;
+		double prom = (n1+n2+n3+n4+n5)/5.0;
+		System.out.println("Promedio de variables creadas:"+ prom);
+
+	}
+
+}
